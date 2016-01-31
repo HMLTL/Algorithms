928821fe554e5ecbfee0b57fd1c0db4658f37cd5
Pikuza Mykola <m.pikuza@tv24.com.ua>
2016-01-31 16:02:54 +0200

    e-olymp-2 digits

--- src/com/pikuza/Main.java
+++ src/com/pikuza/Main.java
@@ -4,17 +4,25 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
+    private static int digitCounter = 0;
 
     public static void main(String[] args)   {
         Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-
         int a = in.nextInt();
-        int b,c;
 
-        b=a/10; c=a%10;
-        out.println(b +" " + c);
+        getCountOfDigits(a);
 
+        out.println(digitCounter);
         out.flush();
+    }
+
+    private static void getCountOfDigits(int input){
+
+        if (input/10>0){
+            getCountOfDigits(input/10);
+        }
+        digitCounter++;
+
     }
 }
