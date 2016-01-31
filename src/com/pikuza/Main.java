785cc7d38da563477cbfd53115691ebe0d8dbeb8
Pikuza Mykola <m.pikuza@tv24.com.ua>
2016-01-31 20:32:46 +0200

    e-olymp-4 two circles

--- src/com/pikuza/Main.java
+++ src/com/pikuza/Main.java
@@ -1,6 +1,7 @@
 package com.pikuza;
 
 import java.io.PrintWriter;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -9,21 +10,41 @@ public class Main {
 
     public static void main(String[] args)   {
         Scanner in = new Scanner(System.in);
+        in.useLocale(new Locale("US"));
         PrintWriter out = new PrintWriter(System.out);
-        int a = in.nextInt();
-//        int a = 4;
+        double x1 = in.nextDouble();
+        double y1 = in.nextDouble();
+        double r1 = in.nextDouble();
+        double x2 = in.nextDouble();
+        double y2 = in.nextDouble();
+        double r2 = in.nextDouble();
+//        int x1 = 0;
+//        int y1 = 0;
+//        int r1 = 5;
+//        int x2 = 5;
+//        int y2 = 0;
+//        int r2 = 1;
 
 
 
-        out.println(getCountOfMatch(a));
+        out.println(getCountOfMatch(x1,y1,r1,x2,y2,r2));
         out.flush();
     }
 
-    private static int getCountOfMatch(int input){
-        if(input == 0) return 0;
-        if(input == 1) return boxMaths;
-        if(input == 2) return boxMaths-add;
-        if(input > 0) return boxMaths*2+add*(input-2);
-        else return 0;
+    private static int getCountOfMatch(double x1, double y1, double r1, double x2, double y2, double r2){
+        double x = x1 - x2;
+        double y = y1 - y2;
+
+//        if(x<0){x= x*(-1);}
+//        if(y<0){y= y*(-1);}
+
+        double d = Math.sqrt(x*x+y*y);
+
+        if(d == r1 + r2) return 1;
+        if(d > r1 + r2) return 0;
+        if(d < r1 + r2) return 2;
+        if(x1 == x2 && y1 == y2 && r1 == r2) return -1;
+
+        return -1;
     }
 }
